ab2973f9c401b6c06bf9e3266312042ad1e1e37f
Voronin Alexandr <info@z-site.ru>
2020-07-21 21:51:58 +0300

    MicroserviceTest Locust | Чтобы в системе был Locust для нагрузочного тестирования

---
+++ MicroserviceTest/locust/locustfile.py
@@ -0,0 +1,10 @@
+import random
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 15)
+
+    @task
+    def index_page(self):
+        #self.client.get('/?filter={"title":"заголовок"}&CACHE_ENABLED=1')
+        self.client.get('/?filter={"title":"заголовок"}')
